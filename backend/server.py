213c380158948e9d24e6f2e4db5a385fcd67d1bb
Andriana <andrianasaffo@gmail.com>
2023-11-14 10:21:12 +0000

    Import modules and create post request
    Import modules needed through flask. Origin for CORS to change to deployed frontend site.
    As no database is present, data temporarily stored in an array that resets when code is changed. Once database is set up real information will be input.
    GET and POST request tested with frontend form to make sure it is working and recieving data.
    Code will need to be refactored once other parts of the frontend and database become available

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={
+     r"/submit_trainee_form": {"origins": "http://localhost:3000"}})
 
 
 @app.route("/")
@@ -8,16 +11,46 @@ def create_server():
     return "Hello server. I am running"
 
 
+trainee_data = {
+    "id": 0,
+    "trainee_name": "Andriana",
+    "github_link": "https://cv-portfolio.onrender.com/",
+    "portfolio_link": "https://cv-portfolio.onrender.com/",
+    "linkedIn_link": "https://cv-portfolio.onrender.com/",
+    "role": "kscn",
+    "about_me": "jskb",
+    "skills": ["hbjccx", "hjvdj"],
+}
+
+all_data = [trainee_data]
+
+
 @app.route("/graduates")
 def graduates():
-    return {"graduates": ["Graduate1", "Graduate2", "Graduate3"]}
+    print(f"All graduates: {trainee_data}")
+    return jsonify(all_data)
 
 
-@app.route("/trainee_form", methods=["POST"])
-def trainee_form():
-    data = request.get_json()
-    print(data)
-    return data
+@app.route("/submit_trainee_form", methods=["GET", "POST"])
+def submit_trainee_form():
+    new_trainee_data = {
+        "id": len(all_data) + 1,
+        "trainee_name": request.json.get("trainee_name"),
+        "github_link": request.json.get("github_link"),
+        "portfolio_link": request.json.get("portfolio_link"),
+        "linkedIn_link": request.json.get("linkedIn_link"),
+        "role": request.json.get("role"),
+        "about_me": request.json.get("about_me"),
+        "skills": request.json.get("skills")
+    }
+
+    if not new_trainee_data["trainee_name"] or not new_trainee_data["github_link"] or not new_trainee_data["portfolio_link"] or not new_trainee_data["linkedIn_link"] or not new_trainee_data["role"] or not new_trainee_data["about_me"] or not new_trainee_data["skills"]:
+        print("Please fill in required fields")
+        return jsonify({'message': 'Please fill in all required fields'}), 400
+
+    all_data.append(new_trainee_data)
+    print(all_data)
+    return jsonify(all_data)
 
 
 if __name__ == "__main__":
