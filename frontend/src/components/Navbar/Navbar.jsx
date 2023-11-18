1d7ca9d51d0a625d70432228ef351d9855c994cb
Mana <managhabraei2022@gmail.com>
2023-11-18 15:37:43 +0000

    navigation bar created

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.css";
+
+
+export const Navbar = () => {
+
+  return( <div className="NavbarContainer">
+    <ul className="nav">
+      <li>Home</li>
+      <li>Graduates</li>
+      <li>Login</li>
+    </ul>
+   </div>);
+};
