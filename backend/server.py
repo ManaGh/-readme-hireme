2a85067686c0aa83f779e4745e10c09a4b53a28e
Andriana <andrianasaffo@gmail.com>
2023-11-21 20:21:06 +0000

    Restructure code and connect DB
    Imports restructured and form submission code refactored to send form data to database created. Form data not sending to database. Connection to be retested

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, jsonify
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import requests
-from dotenv import load_dotenv
-import os
+
+load_dotenv()
 
 
 app = Flask(__name__)
-CORS(app, resources={
-     r"/submit_trainee_form": {"origins": "http://localhost:3000"}})
+CORS(app)
+
+
+# Connection to database
+db_url = os.getenv("DB_URL")
+db_conn = psycopg2.connect(db_url)
+
+INSERT_GRADUATE_RETURN_ID = "INSERT INTO graduates (trainee_name, github_link, portfolio_link, linkedIn_link, role, about_me, skills) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING id"
+GET_GRADUATE_NAME = "SELECT 1 FROM graduates WHERE trainee_name = %s"
+
 
 # Route that hits the root of the server. Use this to make sure the server is running
-
-
 @app.route("/")
 def create_server():
     return "Hello server. I am running"
@@ -26,7 +35,7 @@ trainee_data = {
     "linkedIn_link": "https://www.linkedin.com/in/andriana-saffo/",
     "role": "Full Stack",
     "about_me": "Lorem",
-    "skills": ["HTML", "Python", "JavaScript"],
+    "skills": ["HTML", "Python", "JavaScript", "NodeJS", "CSS", "Flask", "PostgreSQL", "ExpressJS", "React"],
 }
 
 # Submitting form will add graduate data to this array. The data will not persist
@@ -43,9 +52,33 @@ def graduates_list():
     print(f"All graduates: {trainee_data}")
     return jsonify(all_data)
 
+
+# Extracts GitHub username from github_link
+def extract_alphanumeric_details(url):
+    prefix = "https://github.com/"
+
+    # Check if the URL starts with the specified prefix
+    if url.startswith(prefix):
+        # Extract the substring after the prefix
+        details = url[len(prefix):]
+
+        # Filter out non-alphanumeric characters
+        alphanumeric_details = ''.join(
+            char for char in details if char.isalnum())
+
+        return alphanumeric_details
+
+    else:
+        # Return None if the prefix is not present
+        return None
+
+
+# Example usage:
+extracted_result = extract_alphanumeric_details(trainee_data["github_link"])
+print("Extracted result:", extracted_result)
+
+
 # Interacts with GitHub API to pull information stated in query. User login will take users GitHub username
-
-
 @app.route("/graduates", methods=["POST"])
 def graduates():
     try:
@@ -53,39 +86,58 @@ def graduates():
             "Authorization": f"Bearer {gh_token}",
             "Content-Type": "application/json",
         }
-        github_query_json = {
-            "query": '{user(login: "AndrianaOS"){avatarUrl(size: 256), name, bio, email, websiteUrl, repositoriesContributedTo(first: 10){totalCount}, pinnedItems(first: 10) {nodes {... on Repository {name}}}}}'}
+
+        if extracted_result is not None:
+            github_query_json = {
+                "query": f'{{user(login: "{extracted_result}"){{avatarUrl(size: 256), name, bio, email, websiteUrl, repositoriesContributedTo(first: 10){{totalCount}}, pinnedItems(first: 10) {{nodes {{... on Repository {{name}}}}}}}}}}'
+            }
+        else:
+            return jsonify({"error": "Failed to extract login details"}), 400
+
         response = requests.post(
             os.getenv("GITHUB_API_ENDPOINT"), json=github_query_json, headers=GITHUB_HEADERS)
         result = response.json()
+        print(result)
         return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
 # POST request for form
-@app.route("/submit_trainee_form", methods=["GET", "POST"])
+@app.route("/submit_trainee_form", methods=["GET", "POST", "OPTIONS"])
 def submit_trainee_form():
-    new_trainee_data = {
-        "id": len(all_data) + 1,
-        "trainee_name": request.json.get("trainee_name"),
-        "github_link": request.json.get("github_link"),
-        "portfolio_link": request.json.get("portfolio_link"),
-        "linkedIn_link": request.json.get("linkedIn_link"),
-        "role": request.json.get("role"),
-        "about_me": request.json.get("about_me"),
-        "skills": request.json.get("skills")
-    }
+    try:
+        print("Request method:", request.method)
+        print("Request data:", request.get_data())
 
-    if not any(new_trainee_data[key] for key in ["trainee_name", "github_link", "portfolio_link", "linkedIn_link", "role", "about_me", "skills"]):
-        print("Please fill in required fields")
-        return jsonify({'message': 'Please fill in all required fields'}), 400
+        data = request.get_json()
+        print(data)
+        trainee_name = data["trainee_name"]
+        print(trainee_name)
+        github_link = data["github_link"]
+        portfolio_link = data["portfolio_link"]
+        linkedIn_link = data["linkedIn_link"]
+        role = data["role"]
+        about_me = data["about_me"]
+        skills = data["skills"]
+        with db_conn:
+            with db_conn.cursor() as cursor:
+                cursor.execute(GET_GRADUATE_NAME, (trainee_name))
+                graduate_name = cursor.fetchall()[1]
+                graduate_id = cursor.fetchone()[0]
 
-    all_data.append(new_trainee_data)
-    # print(all_data)
-    return jsonify(all_data)
+                if graduate_name:
+                    raise ValueError("Graduate already exists")
+                elif not any(data[key] for key in [trainee_name, github_link, portfolio_link, linkedIn_link, role, about_me, skills]):
+                    raise ValueError("Please fill in required fields")
+                else:
+                    cursor.execute(INSERT_GRADUATE_RETURN_ID, (trainee_name, github_link,
+                                                               portfolio_link, linkedIn_link, role, about_me, skills))
+        db_conn.commit()
+        return jsonify({"id": graduate_id, "message": f"{trainee_name} successfully added"}), 201
+    except Exception as error:
+        print(error)
+        # return {"error": error}, 400
+        error_message = str(error)
+        return jsonify({"error": error_message})
 
-
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
