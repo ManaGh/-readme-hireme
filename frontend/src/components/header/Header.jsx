f6ac75d218b7b733bc9f54dcce8c0dbfbc238d0a
Andriana <andrianasaffo@gmail.com>
2023-11-21 20:21:47 +0000

    Change import path for logo

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import cyflogo from "../images/cyf-logo.png";
-import "./header.css"
+import cyflogo from "../../images/cyf-logo.png";
+import "./header.css";
 
-export const Header = ()=>{
-    return(
-        <header>
-            <img className="cyf-logo" src = {cyflogo} alt = "Code Your Future"/>
-        </header>
-    )
-}
+export const Header = () => {
+  return (
+    <header>
+      <img className="cyf-logo" src={cyflogo} alt="Code Your Future" />
+    </header>
+  );
+};
