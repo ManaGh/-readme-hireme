527ce65445d0eb03255a25a4f3fb680f677f3acc
Andriana <andrianasaffo@gmail.com>
2023-11-22 12:45:35 +0000

    Add another classname to button component
    2nd classname added to distinguish between submit button and other buttons

--- frontend/src/components/shared/Button.jsx
+++ frontend/src/components/shared/Button.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import "./Button.css"
 
 const Button = ({buttonName}) => {
-  return (
-    <button className='btn'>{buttonName}</button>
-  )
+  return <button className="submit-btn landing-btn">{buttonName}</button>;
 }
 
 export default Button
