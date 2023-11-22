6ddfa753af8f4e29cef7abaa77ccabc3d78cfd21
Andriana <andrianasaffo@gmail.com>
2023-11-22 11:49:20 +0000

    Change divs to semantic HTML
    Change divs into other semantic HTML. This makes the code readability easier

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,29 +1,28 @@
-import landingPic from "../images/landingPicture.jpg"
-import githubIcon from "../images/github-mark.svg"
-import "./landing.css" ;
+import landingPic from "../images/landingPicture.jpg";
+import githubIcon from "../images/github-mark.svg";
+import "./landing.css";
 
 export const LandingPage = () => {
-    return (
-        <div className="landing-Container">
-            <div className="firstSection">
-                <img className="landingPic" src = {landingPic} alt="landingPic" />
-                <p className="landingText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores 
-                    ipsam nisi maxime, repellat amet repellendus soluta laborum dignissimos 
-                    porro! Quis tempore libero repudiandae culpa ad architecto quam praesentium 
-                    eius repellendus?
-                    Lorem ipsum dolor sit amet</p>
-            </div>
-            <div className="bottom-section">
-                <div className="section-two">
-                    <span>See Our Wonderful Graduates</span>
-                </div>
-                <div className="section-three">
-                    <img className="githubSvg" src= {githubIcon} alt="" />
-                    <span>Log In With Github</span>
-                </div>
-            </div>
-        </div>
-
-    );
-    
-} 
+  return (
+    <div className="landing-Container">
+      <section className="firstSection">
+        <img className="landingPic" src={landingPic} alt="landingPic" />
+        <p className="landingText">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+          ipsam nisi maxime, repellat amet repellendus soluta laborum
+          dignissimos porro! Quis tempore libero repudiandae culpa ad architecto
+          quam praesentium eius repellendus? Lorem ipsum dolor sit amet
+        </p>
+      </section>
+      <section className="bottom-section">
+        <aside className="section-two">
+          <span>See Our Wonderful Graduates</span>
+        </aside>
+        <aside className="section-three">
+          <img className="githubSvg" src={githubIcon} alt="" />
+          <span>Log In With Github</span>
+        </aside>
+      </section>
+    </div>
+  );
+};
